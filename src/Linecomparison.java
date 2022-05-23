d1916b5844e56cfe0ef44c2759469cbc1672fbf9
Ashwini Rathod <rashwini510@gmail.com>
2022-05-23 22:56:31 +0530

    Added:comparison based on end point

--- src/Linecomparison.java
+++ src/Linecomparison.java
@@ -18,8 +18,11 @@ public class Linecomparison {
     }
 
     public static void checkEquality(double lineOneLength , double lineTwoLength ) {
-        if(lineOneLength == lineTwoLength ) {
+        if (lineOneLength == lineTwoLength) {
             System.out.println("Lines are Equal");
+        }
+        else if(lineOneLength > lineTwoLength) {
+            System.out.println("line1 is greater than line2");
         }
         else {
             System.out.println("Lines are not Equal");
